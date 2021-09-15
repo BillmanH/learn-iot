8040895b5d0db08d2e47fd40a3b351deb4cf25a3
billmanh <william.jeffrey.harding@gmail.com>
2021-09-14 17:20:03 -0700

    found bug in get_storage_info_for_blob

--- devices/camera.py
+++ devices/camera.py
@@ -24,8 +24,7 @@ class device:
         # for iteration, should be set to 0
         self.frame_no = 0
         self.image_no = 0
-        self.blob_info = self.client.get_storage_info_for_blob("camera-images")
-        self.images_path = os.path.join('..','assets','images')
+        self.images_path = os.path.join('assets','images')
 
 
     def wait_for_message(self):
@@ -50,10 +49,11 @@ class device:
             # desired button of your choice
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            self.frame_cap += 1
-            if self.frame_cap==self.frame_cap:
+            self.frame_no += 1
+            if self.frame_no==self.frame_cap:
+                self.frame_no = 0
                 print(f"{self.frame_cap}th frame reached")
-                image_name = self.save_files(self,frame)
+                image_name = self.save_files(frame)
                 self.post_data(image_name)
                 result = self.upload_to_blob(image_name)
                 print(result)
@@ -82,16 +82,18 @@ class device:
 
 
     def upload_to_blob(self,image_name):
-        blob_info = self.client.get_storage_info_for_blob("camera-images")
-        sas_url = "https://{}/{}/{}{}".format(
-                    blob_info["hostName"],
-                    blob_info["containerName"],
-                    blob_info["blobName"],
-                    blob_info["sasToken"]
-                )
-        with BlobClient.from_blob_url(sas_url) as blob_client:
-            with open(os.path.join(self.images_path,image_name), "rb") as f:
-                result = blob_client.upload_blob(f, overwrite=True)
+        try:
+            blob_info = self.client.get_storage_info_for_blob(image_name)
+            sas_url = f"https://{blob_info['hostName']}/{blob_info['containerName']}/{blob_info['blobName']}{blob_info['sasToken']}"
+            with BlobClient.from_blob_url(sas_url) as blob_client:
+                with open(os.path.join(self.images_path,image_name), "rb") as f:
+                    result = blob_client.upload_blob(f, overwrite=True)
+                    blob_client.close()
+                    return result
+        except Exception as e:
+            print("unable to upload : ")
+            return e
+
 
 
 # %%
